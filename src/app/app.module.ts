638412acb1495fd28cddd67b107a6d16eef73602
nasii05 <iamnasih@gmail.com>
2023-09-15 15:53:42 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { CartComponent } from './components/cart/cart.component';
+import { WishlistComponent } from './components/wishlist/wishlist.component';
+import { NavbarComponent } from './components/partials/navbar/navbar.component';
+import { FooterComponent } from './components/partials/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CartComponent,
+    WishlistComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
