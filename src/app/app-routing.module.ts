5bd3472db1e37839de93d75fc56c480ecaf781f1
nasii05 <iamnasih@gmail.com>
2023-09-15 16:16:57 +0530

    basic routing added ⚡

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MenComponent } from './components/men/men.component';
+import { WomenComponent } from './components/women/women.component';
+import { KidsComponent } from './components/kids/kids.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './components/about/about.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: LandingPageComponent},
+  {path:'men', component: MenComponent },
+  {path:'women', component: WomenComponent},
+  {path: 'kids', component: KidsComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'about', component: AboutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,12 @@ import { CartComponent } from './components/cart/cart.component';
 import { WishlistComponent } from './components/wishlist/wishlist.component';
 import { NavbarComponent } from './components/partials/navbar/navbar.component';
 import { FooterComponent } from './components/partials/footer/footer.component';
+import { MenComponent } from './components/men/men.component';
+import { WomenComponent } from './components/women/women.component';
+import { KidsComponent } from './components/kids/kids.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './components/about/about.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +22,13 @@ import { FooterComponent } from './components/partials/footer/footer.component';
     CartComponent,
     WishlistComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    MenComponent,
+    WomenComponent,
+    KidsComponent,
+    ContactComponent,
+    AboutComponent,
+    LandingPageComponent,
   ],
   imports: [
     BrowserModule,
